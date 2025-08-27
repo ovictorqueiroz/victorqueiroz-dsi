120d41b1b08c2674ad9172e4a7e6aa446443e272
ovictorqueiroz <victorbr442@gmail.com>
2025-08-27 20:22:06 -0300

    Introdução a laços while e do... while

--- ProjetoJavaVictorQueiroz.java
+++ ProjetoJavaVictorQueiroz.java
@@ -8,24 +8,19 @@ public class ProjetoJavaVictorQueiroz {
     public static void main(String[] args) {
         // Introdução ao Switch Case
         Scanner input = new Scanner  (System.in);
-        System.out.print("Escolha uma opção de 1 a 3:");
-        int opt = input.nextInt();
-        
-        switch(opt){
-            case 1:
-                System.out.println("Você escolheu a opção 1");
-                break;
-                
-            case 2:
-                System.out.println("Você escolheu a opção 2");
-                break;
-            case 3:
-                System.out.println("Você escolheu a opção 3");
-                break;
-                
-            default:
-                System.out.println("Você escolheu a opção DEFAULT");
-                break;
+        //EXERCICIO 1
+        // crie um programa que exiba na tela os numeros impares entre 100 e 200.
+        int cont = 100;
+        int valor = 100;
+        while(cont <= 200){
+            if (valor  % 2 != 0){
+                System.out.println(valor);
+            }
+            valor++;
+            cont++;
         }
+    
+        }
+        
+   
     }
-}

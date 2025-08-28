c5f292787948e7022551e1f6d606feefd1247c04
ovictorqueiroz <victorbr442@gmail.com>
2025-08-27 22:02:30 -0300

    Introdução a laços while e do... while

--- ProjetoJavaVictorQueiroz.java
+++ ProjetoJavaVictorQueiroz.java
@@ -22,14 +22,44 @@ public class ProjetoJavaVictorQueiroz {
         }
         */
         
-        //EXERCICIO 2
-        // 2 - crie um programa que exiba na tela os numeros de 100 ate 0;
+        /*
+        //EXERCICIO 2 - Crie um programa que exiba na tela os numeros de 100 ate 0;
         
         int cont = 100;
         while(cont >= 0){
             System.out.println(cont);
             cont--;
         }
+        */
+        
+        //
+        //EXERCICIO 3 - receba a idade de n alunos e forneça a média
+        
+        int numAlunos = 0;
+        int somaIdades = 0;
+        int mediaIdades = 0;
+        int cont = 0;
+        
+        int sysOpt = 0;
+        
+        while(sysOpt != 2){
+        System.out.println("Escolha uma opção:\n[1] Calcular média de idade dos alunos\n[2] Sair\nSua escolha: ");
+        sysOpt = input.nextInt();
+            
+            System.out.print("Digite a idade do aluno: ");
+            somaIdades = input.nextInt();
+            somaIdades += somaIdades;
+            numAlunos++;
+        }
+        System.out.println("Saindo do sistema...");
+        /*
+        while(numAlunos <= 4){
+        }
+        
+        mediaIdades = somaIdades / numAlunos;
+        System.out.println("A média de idade do grupo é " + mediaIdades);
+        */
+        
         }
         
    

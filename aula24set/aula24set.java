4c4a8c97cf0d1ac49e96fdbe8c9d974cd0980247
ovictorqueiroz <victorbr442@gmail.com>
2025-09-24 19:45:54 -0300

    criando classe

--- aula24set/aula24set.java
+++
@@ -1,7 +0,0 @@
-package aula24set;
-
-public class Aula24set{
-    public static void main (String [] args){
-
-    }
-}
---
+++ aula24set/aula24set/src/aula24set/Aula24set.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula24set;
+
+/**
+ *
+ * @author Laboratorio-Info
+ */
+public class Aula24set {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}

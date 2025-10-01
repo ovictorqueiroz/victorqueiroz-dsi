3a8d639a2a8c8d5e55e61f27dac7fcfdac16e3ac
ovictorqueiroz <victorbr442@gmail.com>
2025-10-01 19:46:13 -0300

    codando com a marcelly

---
+++ aula01out/src/aula01out/Aula01out.java
@@ -0,0 +1,16 @@
+package aula01out;
+
+
+
+import javax.swing.JOptionPane;
+
+
+
+
+public class Aula01out {
+
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Hello World!", "Mensagem", 1);
+    }
+    
+}

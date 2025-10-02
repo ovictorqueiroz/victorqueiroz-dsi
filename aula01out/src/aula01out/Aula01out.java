b8a42d53cc114571fdd2495f445adec50f7d5740
ovictorqueiroz <victorbr442@gmail.com>
2025-10-01 21:31:11 -0300

    Desafio loteria

--- aula01out/src/aula01out/Aula01out.java
+++ aula01out/src/aula01out/Aula01out.java
@@ -1,16 +1,63 @@
 package aula01out;
-
-
-
+import java.util.Random;
 import javax.swing.JOptionPane;
 
+ class Aula01out {
 
-
-
-public class Aula01out {
-
+ 
     public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null, "Hello World!", "Mensagem", 1);
+        /*
+        double peso, altura, IMC = 0;
+        String temp;
+        
+        temp = JOptionPane.showInputDialog(null, "Digite seu peso em kg (80)", "Peso", 3);
+        peso = Double.parseDouble(temp);
+        
+        temp = JOptionPane.showInputDialog(null, "Digite sua altura em metros (1.58)", "Altura", 3);
+        altura = Double.parseDouble(temp);
+        
+        IMC = (peso / (altura * altura));
+        
+        JOptionPane.showMessageDialog(null, "Seu IMC é " + String.format("%.2f", IMC), "IMC", 1);
+        
+         if( IMC <= 18.5){
+            JOptionPane.showMessageDialog(null, "Você está abaixo do peso", "IMC", 1);    
+             
+             
+         }else if ( IMC <= 25 ){
+            JOptionPane.showMessageDialog(null, "Você está no peso ideal", "IMC", 1);    
+             
+         }else if ( IMC <= 30){
+            JOptionPane.showMessageDialog(null, "Você está acima do peso", "IMC", 1);    
+             
+         }else{
+            JOptionPane.showMessageDialog(null, "Você está obeso", "IMC", 1);    
+         }  
+        */
+        
+        //DESAFIO - LOTERIA
+        Random random = new Random();
+        //Menu
+        int menuOpt = 0;
+        
+        switch(menuOpt){
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            default:
+                JOptionPane.showMessageDialog(null, "Opção Inválida!", "Mensagem", 0);
+                
+        }
+        
+        
+        
     }
     
 }

b4a274937220858c6e8dc1390b13d86eb0735434
ovictorqueiroz <victorbr442@gmail.com>
2025-10-29 20:22:35 -0300

    feat(exercicioQuatro): Inicializando com Interface Gráfica

--- Aula2910/src/aula2910/Aula2910.java
+++ Aula2910/src/aula2910/Aula2910.java
@@ -1,21 +1,35 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package aula2910;
-
+import javax.swing.JOptionPane;
 /**
  *
  * @author Laboratorio-Info
  */
 public class Aula2910 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int [] codVend = {1,2,3,4,5,6,7,8,9,10};
+        String [] nomeVend = {"Pedro", "Daniele", "Augusto", "Emily", "Tiago", "Rosana", "Alisson", "Meire", "Thales", "Valéria"};
+        double [] totVnd = new double [10];
+        double [] cmsVnd = new double [10];
+	double [] vlrReceber = new double [10];
+        String print = "";
+        String print2 = "";
+        
+        
+        for(int i = 0; i < 10; i++){
+            String temp = JOptionPane.showInputDialog(null, "Cód: " + codVend[i] + " | Vendedor: " + nomeVend[i] + " | Digite o total da sua venda: ");
+            totVnd[i] = Double.parseDouble(temp);
+            print += totVnd[i] + " | ";
+            
+            temp = "";
+            
+            temp = JOptionPane.showInputDialog(null, "Digite o % da comissão:");
+            cmsVnd[i] = Double.parseDouble(temp);
+            print2 += cmsVnd[i] + " | ";
+            
+        }
+            JOptionPane.showMessageDialog(null, print);
+            JOptionPane.showMessageDialog(null, print2);
     }
     
 }
